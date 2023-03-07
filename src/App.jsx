245af11bf8330a59dd63db66c6e92d7a2787dc59
tepavcevic <djordje.tepa@gmail.com>
2023-03-07 08:58:28 +0100

    Added data.js and mapping card objects in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
 import './App.css'
+import { data } from './data'
 import Card from './components/Card'
 
 function App() {
 
   return (
     <main className="content">
-      <Card 
-        cardHeader="Free"
-        price="$0"
-        amountOfStorage="2"
-        numberOfUsers="10"
-        supportTier="Email"
-        buttonText="Sign up for free"
-      />
+      
+      {data.map(data => 
+        <Card 
+        cardHeader={data.cardHeader}
+        price={data.price}
+        amountOfStorage={data.amountOfStorage}
+        numberOfUsers={data.numberOfUsers}
+        supportTier={data.supportTier}
+        buttonText={data.buttonText}
+        classNameHeader={data.classNameHeader}
+        classNameCard={data.classNameCard}
+      />)}
 
-      <Card 
-        cardHeader="Pro"
-        price="$15"
-        numberOfUsers="20"
-        amountOfStorage="10"
-        supportTier="Priority email"
-        buttonText="Get started"
-      />
-
-      <Card 
-        cardHeader="Enterprise"
-        price="$29"
-        numberOfUsers="30"
-        amountOfStorage="15"
-        supportTier="Phone an email"
-        buttonText="Contact us"
-        classNameHeader="card-header-highlighted"
-        classNameCard="card-highlighted"
-      />
     </main>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        cardHeader: "Free",
+        price: "$0",
+        amountOfStorage: "2",
+        numberOfUsers: "10",
+        supportTier: "Email",
+        buttonText: "Sign up for free",
+        classNameHeader: "",
+        classNameCard: "",
+    },
+    {
+        cardHeader: "Pro",
+        price: "$15",
+        amountOfStorage: "10",
+        numberOfUsers: "20",
+        supportTier: "Priority email",
+        buttonText: "Get started",
+        classNameHeader: "",
+        classNameCard: "",
+    },
+    {
+        cardHeader: "Enterprise",
+        price: "$29",
+        amountOfStorage: "15",
+        numberOfUsers: "30",
+        supportTier: "Phone and email",
+        buttonText: "Contct us",
+        classNameHeader: "card-header-highlighted",
+        classNameCard: "card-highlighted",
+    }
+]
