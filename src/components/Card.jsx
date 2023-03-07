562e1939707843235ebcf1cfdb4bc8d91918582c
tepavcevic <djordje.tepa@gmail.com>
2023-03-07 10:09:35 +0100

    Refactored for simpler code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,9 @@
 export default function Card({ priceTier, cardHeader, price, numberOfUsers, amountOfStorage, supportTier, buttonText }) {
     return(
-        <div>
-            {priceTier !== "enterprise" ?
-            <div className="card">
-            {
-                priceTier !== "enterprise" ? 
-                <header className="card-header">
+            <div className={`card ${priceTier === "enterprise" && "card-highlighted"}`}>
+                <header className={`card-header ${priceTier === "enterprise" && "card-header-highlighted"}`}>
                     <h2 className="card-title">{cardHeader}</h2>
-                </header> :
-                <header className="card-header card-header-highlighted">
-                <h2 className="card-title">{cardHeader}</h2>
                 </header>
-            }
             <div className="card-body">
                 <div className="card-price">
                     <h3 className="card-price-title">{price}
@@ -25,42 +17,8 @@ export default function Card({ priceTier, cardHeader, price, numberOfUsers, amou
                         <li>Help center access</li>
                     </ul>
                 </div>
-                {buttonText === "Sign up for free" ? 
-                <button className="btn btn-free">{buttonText}</button> :
-                <button className="btn btn-paid">{buttonText}</button>
-                 }
+                <button className={`btn ${priceTier === "free" ? "btn-free" : "btn-paid"}`}>{buttonText}</button>
             </div>
-        </div> :
-        <div className="card card-highlighted">
-        {
-            priceTier !== "enterprise" ? 
-            <header className="card-header">
-                <h2 className="card-title">{cardHeader}</h2>
-            </header> :
-            <header className="card-header card-header-highlighted">
-            <h2 className="card-title">{cardHeader}</h2>
-            </header>
-        }
-        <div className="card-body">
-            <div className="card-price">
-                <h3 className="card-price-title">{price}
-                <span className="price-title-small text-muted">/mo</span></h3>
-            </div>
-            <div className="services">
-                <ul>
-                    <li>{numberOfUsers} Users included</li>
-                    <li>{amountOfStorage} GB of storage</li>
-                    <li>{supportTier} support</li>
-                    <li>Help center access</li>
-                </ul>
-            </div>
-            {buttonText === "Sign up for free" ? 
-            <button className="btn btn-free">{buttonText}</button> :
-            <button className="btn btn-paid">{buttonText}</button>
-             }
-        </div>
-    </div>
-        }
         </div>
     )
 }
